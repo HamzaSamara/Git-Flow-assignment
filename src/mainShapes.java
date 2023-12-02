b20a0d1d93357fa8872830c5b6fcce3fbee2e0c2
HamzaSamara <hamzasamara9@gmail.com>
2023-12-02 18:17:24 +0300

    Initial commit

---
+++ src/mainShapes.java
@@ -0,0 +1,29 @@
+public class mainShapes {
+    public static void main(String args[]){
+        /*
+        Circle c1 = new Circle(5);
+        System.out.println("Circle Radius = " + c1.getRadius());
+        System.out.println("Area = " + c1.Area());
+        System.out.println("Perimeter = " + c1.Perimeter());
+
+        System.out.println("########################################");
+
+
+        Square s1 = new Square(4);
+        System.out.println("Square Side = " + s1.getSide());
+        System.out.println("Area = " + s1.Area());
+        System.out.println("Perimeter = " + s1.Perimeter());
+
+        System.out.println("########################################");
+
+
+        Rectangle r1 = new Rectangle(4,3);
+        System.out.println("Rectangle Height = " + r1.getHeight() + " --- Rectangle Width = " + r1.getWidth());
+        System.out.println("Area = " + r1.Area());
+        System.out.println("Perimeter = " + r1.Perimeter());
+
+        System.out.println("########################################");
+        */
+
+    }
+}
